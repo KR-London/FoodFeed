6c08aeaabbc9d67ce7fb24253186d899db6e564d
Daniel Haight <daniel@haight.io>
2020-08-27 17:28:56 +0100

    Initial Commit

---
+++ FoodFeed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FoodFeed
+//
+//  Created by Daniel Haight on 27/08/2020.
+//  Copyright © 2020 Daniel Haight. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
