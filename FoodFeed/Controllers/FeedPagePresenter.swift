2e94a5b2df5fe07cf49095ce9ec5bf29e476f743
kate <ekaterina.roberts@gmail.com>
2020-09-28 09:58:04 +0100

    first post  adjusted

--- FoodFeed/Controllers/FeedPagePresenter.swift
+++ FoodFeed/Controllers/FeedPagePresenter.swift
@@ -37,7 +37,7 @@ class FeedPagePresenter: FeedPagePresenterProtocol{
        // configureAudioSession()
         feeds = [Feed(id: 0, text: "Swipe!", image:nil), Feed(id: 1, text: nil, image: "one.jpeg"), Feed(id: 2, text: "Stay curious!" , image: nil), Feed(id: 3, text: nil, image: "three.jpeg" ), Feed(id: 4, text: "I might try this and think it's not bad.", image: nil), Feed(id: 5, text: nil, image: "two.jpeg"), Feed(id: 6, text: "Party today!", image: nil )]
         fetchFeeds()
-        let initialFeed = Feed(id: 1, text: nil, image: "one.jpg")
+        let initialFeed = Feed(id: 0, text:  "Swipe!", image: nil)
         view.presentInitialFeed(initialFeed)
     }
     
